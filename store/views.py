3477e57ebf749a6868cf94cae990adf61b9f74fc
khadija-codencoit <khadija.khatun@codencoit.com>
2026-01-19 15:02:28 +0600

    add permissions for ProductViewSet

--- store/views.py
+++ store/views.py
@@ -14,6 +14,7 @@ from rest_framework import status
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.viewsets import ModelViewSet, GenericViewSet
 from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from .pagination import *
 from .filters import ProductFilter
 
@@ -22,12 +23,14 @@ from .filters import ProductFilter
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    filter_backends = [DjangoFilterBackend,SearchFilter,OrderingFilter]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_class = ProductFilter
     pagination_class = DefaultPagination
-    search_fields = ['title','description']
+    search_fields = ['title', 'description']
     ordering_fields = ['unit_price']
+    permission_classes = [IsAuthenticated]
 
+   
 
     # def get_queryset(self):
     #     queryset = Product.objects.all()
