96a5340ace6f08c0f877c6074d26dd99eb243dfb
khadija-codencoit <khadija.khatun@codencoit.com>
2026-02-17 17:55:51 +0600

    Write test case for collection validation error

--- store/test/test_collections.py
+++ store/test/test_collections.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from rest_framework import status
 from rest_framework.test import APIClient
 import pytest
@@ -17,6 +18,16 @@ class TestCreateCollection:
 
         # Assert
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+        def test_if_data_is_invalid_returns_401(self):
+   
+            client = APIClient()
+            client.force_authentication(user = User(is_staff=True))
+            response = client.post('/store/collections/', {'title': ''})
+            assert response.status_code == status.HTTP_400_BAD_REQUEST
+            assert response.data['title'] is not None
+
+       
 
 
 
